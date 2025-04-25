f7e2baba24e2c9e2ad08148025eade4a5f388e89
Lazare Rossillon <lazrossi@student.42.fr>
2025-04-25 12:23:12 +0200

    update mdx components

--- app/mdx-components.tsx
+++ app/mdx-components.tsx
@@ -1,8 +1,8 @@
+import { Tabs as CustomTabs, Tab } from '@/components/fumadocs/tabs';
 import { Banner } from 'fumadocs-ui/components/banner';
 import { Callout } from 'fumadocs-ui/components/callout';
 import { Card, Cards } from 'fumadocs-ui/components/card';
 import { Step, Steps } from 'fumadocs-ui/components/steps';
-import { Tabs } from 'fumadocs-ui/components/tabs';
 import type { MDXComponents } from 'mdx/types';
 
 // Export components for MDX
@@ -10,7 +10,8 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     return {
         ...components,
         // Basic components
-        Tabs: Tabs,
+        Tabs: CustomTabs,
+        Tab: Tab,
         Steps: Steps,
         Step: Step,
         Callout: Callout,
